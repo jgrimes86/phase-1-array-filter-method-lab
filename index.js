eecafb9a08748a8adcfa5875441c4078ffca7c83
Jim Grimes <jimgrimes86@gmail.com>
2023-09-13 16:31:34 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(driverList, nameString) {
+    return driverList.filter(function (driverItem) {
+        return (driverItem === nameString) || (driverItem.toLowerCase() === nameString.toLowerCase()) 
+    })
+}
+// console.log(findMatching(drivers, "bobby"))
+
+function fuzzyMatch(driverList, nameString) {
+    return driverList.filter(function (driverItem) {
+        return driverItem[0] === nameString[0]
+    })
+}
+
+function matchName(driverList, nameString) {
+    return driverList.filter(function (driverItem) {
+        return driverItem.name === nameString
+    })
+}
